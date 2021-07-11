2474193147f5ec12028359d333a477a4388bd8b0
Alex ONeill <73564404+alex-oneill@users.noreply.github.com>
2021-07-11 14:25:38 -0400

    Reworked formatting

--- project_main.py
+++ project_main.py
@@ -1,47 +1,67 @@
+"""
+Database Management Systems - Prof. Scharff (Summer 2021)
+Final Project
+Alex ONeill
+Maryia Kalodkina
+"""
 import psycopg2
 from configparser import ConfigParser
-#from tabulate import tabulate
 
-#con = psycopg2.connect(
-#    host="localhost",
-#    database="production",
-#    user="postgres",
-#    password="*****")
 
+# SECTION: FUNCTIONS
 def config(filename='./database.ini', section='postgres'):
-    """Establishes connection to local postgres db with masked credentials."""
+    """
+    Ingests database connection parameters with masked credentials from a database.ini file in the below format:
+        [postgres]
+        host=localhost
+        database=database_name
+        user=username
+        password=password
+    """
     parser = ConfigParser()
     parser.read(filename)
-    db_conn = {}
+    db_config = {}
     for param in parser.items(section):
-        db_conn[param[0]] = param[1]
-    return db_conn
+        db_config[param[0]] = param[1]
+    return db_config
 
 
-try:
-    params = config()
-    conn = psycopg2.connect(**params)
-    #For isolation: SERIALIZABLE
-    conn.set_isolation_level(3)
-    #For atomicity
-    conn.autocommit = False
-    cur = conn.cursor()
-    # QUERY
-    cur.execute("""SELECT p.p_name
-            FROM product p, stock s
-            WHERE p.prod_id = s.prod_id 
-            AND s.dep_id = 'd2';""")
-    data = cur.fetchall()
-    print(data)
-    
-    
+def connect(con_str):
+    """Establishes connection to the database"""
+    try:
+        connection = psycopg2.connect(**con_str)
+        return connection
+    except Exception as conn_err:
+        print(conn_err)
+        print('Unable to connect to database. Aborting')
 
-except (Exception, psycopg2.DatabaseError) as err:
-    print(err)
-    print("Transactions could not be completed so database will be rolled back before start of transactions")
-    conn.rollback()
-finally:
-    if conn:
+
+# SECTION: MAIN
+params = config()
+conn = connect(params)
+cur = conn.cursor()
+
+if conn:
+    try:
+        # NOTE: Set to serialize for transaction ISOLATION
+        conn.set_isolation_level(3)
+
+        # NOTE: No autocommit to instill ATOMICITY
+        conn.autocommit = False
+
+        # TODO: QUERY
+        cur.execute("""SELECT p.p_name
+                FROM product p, stock s
+                WHERE p.prod_id = s.prod_id
+                AND s.dep_id = 'd2';""")
+        # TODO: REMOVE THESE
+        data = cur.fetchall()
+        print(data)
+    except (Exception, psycopg2.DatabaseError) as err:
+        print(err)
+        print("Transactions could not be completed so database will be rolled back before start of transactions")
+        conn.rollback()
+    finally:
         conn.commit()
         cur.close
         conn.close
